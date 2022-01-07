413a536a8ccc9ea44243dc6a14191ef603f069a9
lybueno <lybueno@gmail.com>
2022-01-07 20:06:26 -0300

    Consulta de notificações DSLearn

--- dslearn/backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
+++ dslearn/backend/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
@@ -5,8 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslearnbds.dto.NotificationDTO;
@@ -20,8 +20,10 @@ public class NotificationResource {
 	private NotificationService service;
 	
 	@GetMapping
-	public ResponseEntity<Page<NotificationDTO>> notificationsForCurrentUser(Pageable pageable){
-		Page<NotificationDTO> page = service.notificationsForCurrentUser(pageable);
+	public ResponseEntity<Page<NotificationDTO>> notificationsForCurrentUser(
+			@RequestParam(name = "unreadOnly", defaultValue = "false") Boolean unreadOnly,
+			Pageable pageable){
+		Page<NotificationDTO> page = service.notificationsForCurrentUser(unreadOnly, pageable);
 		return ResponseEntity.ok().body(page);
 	}
 }
