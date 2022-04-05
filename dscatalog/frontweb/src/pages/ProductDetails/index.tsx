9b3eb4a2efb5034481db4ea12a62a7ae6d4b2126
Lygia Bueno <lybueno@gmail.com>
2022-04-05 18:06:18 -0300

    ProductDetails layout mobile

--- dscatalog/frontweb/src/pages/ProductDetails/index.tsx
+++ dscatalog/frontweb/src/pages/ProductDetails/index.tsx
@@ -1,8 +1,37 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg'
+import ProductPrice from 'components/ProductPrice';
+
+import './styles.css'
+
 const ProductDetails = () => {
 
     return (
 
-        <h1>Product Details</h1>
+        <div className="product-details-container">
+            <div className="base-card product-details-card">
+                <div className="goback-container">
+                    <ArrowIcon />
+                    <h2>Voltar</h2>
+                </div>
+                <div className="row">
+                    <div className="col-xl-6">
+                        <div className="img-container">
+                            <img src='https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg' alt='Imagem Produto' />
+                        </div>
+                        <div className="name-price-container">
+                            <h1>Nome do Produto</h1>
+                            <ProductPrice price={1234.56}/>
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <div className="description-container">
+                            <h2>Descrição do produto</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, enim.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
