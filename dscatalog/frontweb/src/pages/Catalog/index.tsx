3ef82a6a3bf78eafc48dd5d428c9bec8f8c335ef
Lygia Bueno <lybueno@gmail.com>
2022-04-06 17:08:21 -0300

    Catalog title

--- dscatalog/frontweb/src/pages/Catalog/index.tsx
+++ dscatalog/frontweb/src/pages/Catalog/index.tsx
@@ -2,6 +2,8 @@ import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
 
+import './styles.css';
+
 const Catalog = () => {
   const product: Product = {
     id: 2,
@@ -25,8 +27,11 @@ const Catalog = () => {
   };
 
   return (
-    <div className="container my-4">
-      <div className="row">
+    <div className="container my-4 catalog-container">
+      <div className='row'>
+        <h1>Catálogo de produtos</h1>
+      </div>
+      <div className="row catalog-title-container">
         <div className="col-sm-6 col-lg-4 col-xl-3">
           <Link to="/products/1">
             <ProductCard product={product} />
