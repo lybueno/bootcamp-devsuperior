1ecd021f9275f375c1bf054bda58727789651ea1
Lygia Bueno <lybueno@gmail.com>
2022-05-18 16:26:28 -0300

    Auth routes

--- dscatalog/frontweb/src/Routes.tsx
+++ dscatalog/frontweb/src/Routes.tsx
@@ -1,5 +1,6 @@
 import Navbar from "components/Navbar";
 import Admin from "pages/Admin";
+import Auth from "pages/Admin/Auth";
 import Catalog from "pages/Catalog";
 import Home from "pages/Home";
 import ProductDetails from "pages/ProductDetails";
@@ -20,6 +21,10 @@ const Routers = () => {
                 </Route>
                 <Route path="/products/:productId">
                     <ProductDetails />
+                </Route>
+                <Redirect from="/admin/auth" to="/admin/auth/login" exact />
+                <Route path="/admin/auth">
+                    <Auth />
                 </Route>
                 <Redirect from="/admin" to="/admin/products" exact />
                 <Route path="/admin">
