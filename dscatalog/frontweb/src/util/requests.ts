02b95b2996ec4e8f849d010418e94c794a901805
Lygia Bueno <lybueno@gmail.com>
2022-07-20 21:50:04 -0300

    Saving auth data to localStorage

--- dscatalog/frontweb/src/util/requests.ts
+++ dscatalog/frontweb/src/util/requests.ts
@@ -1,7 +1,18 @@
 import axios from 'axios';
 import qs from 'qs';
 
+type LoginResponse = {
+    access_token: string,
+    token_type: string,
+    expires_in: number,
+    scope: string,
+    userFirstName: string,
+    userId: number
+} 
+
 export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
+const tokenKey = 'authData';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
@@ -20,10 +31,21 @@ export const requestBackendLogin = (loginData : LoginData) => {
         Authorization: basicHeader()
     }
 
+    // stringify = eh para montar a query string dos argumentos da requisicao a partir de um objeto
     const data = qs.stringify({
         ...loginData,
         grant_type: 'password'
     });
 
     return axios({method: 'POST', baseURL: BASE_URL, url: '/oauth/token', data, headers})
+}
+
+export const saveAuthData = (obj : LoginResponse) => {
+    // stringify = transformar o objeto em JSON na forma de texto
+    localStorage.setItem(tokenKey, JSON.stringify(obj));
+}
+
+export const getAuthData = () => {
+    const str = localStorage.getItem(tokenKey) ?? "{}";
+    return JSON.parse(str) as LoginResponse;
 }
