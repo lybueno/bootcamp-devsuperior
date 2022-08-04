a86d69bdbec20a346fc9099d38ef7be5967db93a
Lygia Bueno <lybueno@gmail.com>
2022-08-03 21:12:34 -0300

    Check user authenticated

--- dscatalog/frontweb/src/util/history.ts
+++ dscatalog/frontweb/src/util/history.ts
@@ -1,3 +1,6 @@
 import { createBrowserHistory } from 'history';
 
 export default createBrowserHistory();
+
+// tive problemas com o modulo history e precisei installar a biblioteca history na versao 4.10.1 para ter compatibilidade com o componente history do Router
+// [yarn add history@4.10.1]
--- dscatalog/frontweb/src/util/requests.ts
+++ dscatalog/frontweb/src/util/requests.ts
@@ -1,7 +1,15 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 import history from './history';
+import jwtDecode from  'jwt-decode'
 
+type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+
+type TokenData = {
+    exp: number;
+    user_name: string;
+    authorities: Role[]
+}
 
 type LoginResponse = {
     access_token: string,
@@ -86,3 +94,17 @@ axios.interceptors.response.use(function (response) {
     // Do something with response error
     return Promise.reject(error);
   });
+
+  export const getTokenData = () : TokenData | undefined => {
+    const loginResponse = getAuthData();
+    try{
+        return jwtDecode(loginResponse.access_token) as TokenData;
+    } catch(error){
+        return undefined;
+    }    
+  }
+
+  export const isAuthenticated = () : boolean => {
+    const tokenData = getTokenData();
+    return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
+  }
